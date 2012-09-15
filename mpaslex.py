8efb74e53a38649ecb065d2ecb9890bf6e55da41
Jona <jalvarez@tamarindolabs.com>
2012-09-15 14:33:10 -0500

    Sync

--- mpaslex.py
+++ mpaslex.py
@@ -16,9 +16,57 @@ keywords = (
 )
 
 tokens = keywords + (
-	'LT', 'LE', 'GT', 'GE', 'EQ', 'NE', 'MAS', 'MENOS', 'DIV', 'MUL', 'PARI', 'PARD', 'COMA', 'DPUN', 'CORI', 'CORD', 'PCOMA', 'PUN'
+	'LT', 'LE', 'GT', 'GE', 'EQ', 'NE', 'MAS', 'MENOS', 'DIV', 'MUL', 'PARI', 'PARD', 'COMA', 'DPUN', 'CORI', 'CORD', 'PCOMA', 'PUN', 'ASIG', 'INUM', 'FNUM', 'STRING', 'COMEN'
 )
 
+t_ignore = ' \t'
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9]_'
+    if t.value in keywords:
+        t.type = t.value
+    return t
+
+t_LT = r'<'
+t_LE = r'<='
+t_GT = r'>'
+t_GE = r'=>'
+t_EQ = r'='
+t_NE = r'!='
+t_MAS = r'\+'
+t_MENOS = r'-'
+t_DIV = r'/'
+t_MUL = r'\*'
+t_PARI = r'\('
+t_PARD = r'\)'
+t_COMA = r'\,'
+t_DPUN = r':'
+t_CORI = r'\['
+t_CORD = r'\]'
+t_PCOMA = r';'
+t_PUN = r'\.'
+t_ASIG = r':='
+
+t_FNUM = r'((\d*\.\d+)(E[\+-]?\d+)?|([1-9]\d*E[\+-]?\d+))'
+t_STRING = r'\".\"'
+
+# TODO: Manejo de errores
+t_COMEN = r'/\*(.|\n|\"|\\)*?\*/'
+
+def t_NEWLINE(t):
+    r'\n'
+    t.lexer.lineno += 1
+    return t
 
 
+def t_error(t):
+    print("Illegal character %s" % t.value[0])
+    t.lexer.skip(1)
+
+lex.lex()
+
+for tok in lexer:
+    print tok
+
+print "Numero de lineas: %i" % tok.lexer.lineno
 
---
+++ test.pas
@@ -0,0 +1,31 @@
+fun main()
+	v:int[8192];
+	i:int;
+	n:int;
+begin
+	print("Entre n: ");
+	read(n);
+	i := 0;
+	while i<n do
+	begin
+		read(v[i]);
+		i := i+1
+	end
+	quicksort(0, n-1, v);
+	i := 0;
+	while i<n-1 do
+	begin
+		write(v[i]); print(" ");
+		if 0 < v[i] - v[i+1] then
+		begin
+			print("Quicksort falló");
+			write(i);
+			print("\n");
+			return(0)
+		end
+		else
+			i:=i+1
+	end
+	write(v[i]);
+	print("Éxito\n)
+end	
