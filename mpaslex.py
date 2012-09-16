c2b9fc3ef567c083553313924e5a2972d5352bc2
Jona <jalvarez@tamarindolabs.com>
2012-09-16 11:35:17 -0500

    Archivos como parámetro + Validaciones + Excepciones I/O

--- mpaslex.py
+++ mpaslex.py
@@ -9,7 +9,10 @@
 # Licence:     Free share
 #-----------------------------------------------------------------------------
 
+import sys
 import ply.lex as lex
+
+debug = False
 
 keywords = (
 	'INT', 'FLOAT', 'WHILE', 'IF', 'THEN', 'ELSE', 'BEGIN', 'DO', 'END', 'PRINT', 'WRITE', 'READ', 'SKIP', 'RETURN', 'BREAK', 'AND', 'OR', 'NOT', 'FUN', 'ID',
@@ -27,9 +30,9 @@ def t_ID(t):
         t.type = t.value
     return t
 
-t_LT = r'<'
+t_LT = r'\<'
 t_LE = r'<='
-t_GT = r'>'
+t_GT = r'\>'
 t_GE = r'=>'
 t_EQ = r'='
 t_NE = r'!='
@@ -63,10 +66,27 @@ def t_error(t):
     print("Illegal character %s" % t.value[0])
     t.lexer.skip(1)
 
-lex.lex()
+# Build
 
-for tok in lexer:
-    print tok
+if len(sys.argv) > 1:
+    lexer = lex.lex()
+    try:
+        dato = open(sys.argv[1], "r")
+    except IOError as e:
+        print ("I/O error({0}): {1}".format(e.errno, e.strerror))
 
-print "Numero de lineas: %i" % tok.lexer.lineno
+    else:
+        if debug : print(dato.read())
 
+        lexer.input(dato.read())
+        dato.close()
+
+        # Tokenize
+        for tok in lexer:
+            print (tok)
+
+        print ( "Numero de lineas: %i" % tok.lexer.lineno )
+
+else:
+    print ("File argument expected. Usage:")
+    print ("python mpaslex.py <file>")
