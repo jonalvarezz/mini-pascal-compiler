797457cf6d245a8777353806d222acea98089214
Jona <jalvarez@tamarindolabs.com>
2012-10-07 01:22:29 -0500

    Correccion entre hijos y hojas al crear Nodos, Reglas ordenadar segun el grammar y algunas corregidas segun commit 6a6ac1284ec4c6f789b1503c7f55e8d28ebb465a

--- mpasparse.py
+++ mpasparse.py
@@ -55,161 +55,169 @@ def dump_tree(node, indent = ""):
 		else:
 			dump_tree(c, indent + "  |-- ")
 
-# -----------------------------------------------------------------------------
-# Parser
-# -----------------------------------------------------------------------------
-def type_float():
-	pass
+#  ---------------------------------------------------------------
+#  LOCATION
+#  ---------------------------------------------------------------
 
-#-------------type-------------------
-def p_type(p):
+def p_location_1(p):
+	'location: ID'
+	p[0] = Node('ID',[],p[1])
+
+def p_location_2(p):
+	'location: ID CORI expre CORD'
+	p[0] = Node('ID[]',[],p[1])
+
+#  ---------------------------------------------------------------
+#  RELACION
+#  ---------------------------------------------------------------
+
+def p_relacion_condicion(p):
+    'relacion: expre condicion expre'
+     p[0] = Node('relacion_condicion', [p[1],p[3]])
+
+def p_relacion_lt(p):
+	'relacion: expre LT expre'
+	 p[0] = Node('<',[p[1],p[3]])
+
+def p_relacion_le(p):
+	'relacion: expre LE expre'
+	 p[0] = Node('<=',[p[1],p[3]])
+
+def p_relacion_gt(p):
+	'relacion: expre GT expre'
+	 p[0] = Node('>',[p[1],p[3]])
+
+def p_relacion_ge(p):
+	'relacion: expre GE expre'
+	 p[0] = Node('>=',[p[1],p[3]])
+
+def p_relacion_eq(p):
+	'relacion: expre EQ expre'
+	 p[0] = Node('=',[p[1],p[3]])
+
+def p_relacion_ne(p):
+	'relacion: expre NE expre'
+	 p[0] = Node('!=',[p[1],p[3]])	 
+
+def p_relacion_and(p):
+	'relacion: relacion AND relacion'
+	 p[0] = Node('and',[p[1],p[3]])
+
+def p_relacion_or(p):
+	'relacion: relacion OR relacion'
+	 p[0] = Node('or',[p[1],p[3]])
+
+def p_relacion_not(p):
+	'relacion: NOT relacion'
+	p[0] =node('not',[p[2]])
+
+def p_relacion_parent(p):
+	'relacion: PARI relacion PARD'
+	p[0] =node('relacion',[p[2]])
+
+#  ---------------------------------------------------------------
+#  TYPE
+#  ---------------------------------------------------------------
+
+def p_type_f(p):
 	'type: FLOAT'
 	p[0] = Node('FLOAT',[],p[1])
 
-def p_type(p):
+def p_type_i(p):
 	'type: INT'
 	p[0] = Node('INT',[],p[1])
 
-def p_type(p):
+def p_type_fa(p):
 	'type: FLOAT CORI expre CORD'
 	p[0] = Node('FLOAT_Array',[],p[3])
 
-def p_type(p):
+def p_type_ia(p):
 	'type: INT CORI expre CORD'
 	p[0] = Node('INT_Array',[],p[3])
 
-#---------------exprelist---------------------------------------
-def exprelist_coma():
-   ' exprelist: expre ; exprelist'
-   p[1].append(p[3])
-	p[0] = p[1]
+#  ---------------------------------------------------------------
+#  EXPRLIST
+#  ---------------------------------------------------------------
 
-def exprelist_():
-	'exprelist: expre'
-	p[0] = p[1]
-	
-#---------------expre----------------------------------------------
-
-def p_expre_mas(p):
-	'expre: expre MAS expre'
-	p[0]= Node('+',p[2],p[3])
-
-
-def p_expre_menos(p):
-	'expre: expre MENOS expre'
-	p[0]= Node('-',p[2],p[3])
-
- 
-def p_expre_mul(p):
-	'expre: expre MUL expre'
-	p[0]= Node('*',p[2],p[3])
-
-
-def p_expre_div(p):
-	'expre: expre DIV expre'
-	p[0]= Node('/',p[2],p[3])
-
- 
-def p_expre_masu(p):
-	'expre: MAS expre'
-	p[0]= Node('',p[2])
-
-
-def p_expre_menosu(p):
-	'expre: MENOS expre'
-	p[0]= Node('-',p[2])
-
-
-# def p_expre_call(p):
-# 	'expression : ID PARI expre PARD'
-#  	p[0]= Node('call',p[1],p[3])
-
-
-def p_expre_id(p):
-	'expre: ID'
-	p[0] = Node('id',p[1],[])
-
-
-def p_expre_fnum(p):
-	'expre: FNUM'
-	p[0]= Node(numero_flotante,p[1])
-
-
-def p_expre_inum(p):
-	'expre: INUM'
-	p[0]= Node(numero,p[1])
-
-
-def p_expre_cast_int(p):
-	'expre: ID CORI exprelist CORD'
-	p[0] = Node('cast_int',p[3],p[])
-
-def p_expre_cast_float(p):
-	'expre: ID CORI exprelist CORD'
-	p[0] = Node('cast_float',p[3],p[])
-
-def p_expre_mul2(p):
-	'expre: ID CORI exprelist CORD'
-	p[0] = Node(mul_cor_fum,p[1],p[3])
-	if p[3].typ != 'int'
-	 print "!ERROR¡ se esperava un entero" 
-
-
-#---------------exprelist---------------------------------------
 def p_exprelist_coma(p):
-   ' exprelist: expre ; exprelist'
-   p[1].append(p[3])
-	p[0] = p[1]
+   'exprelist: exprelist COMA expre'
+	p[0] = p[1].append(p[3])
 
 def p_exprelist_(p):
 	'exprelist: expre'
 	p[0] = p[1]
-#---------------condicion--------------------------------------
-def p_condicion_LT(p):
-   'condicion: LE'
-   P[0]= Node(menor_que,p[1],p[])
-
-def p_condicion_LE(p):
-   'condicion: LE'
-   P[0]= Node(menor_que,p[1],p[])
 	
-def p_condicion_GT(p):
-   'condicion: LE'
-   P[0]= Node(menor_que,p[1],p[])
-	
-def p_condicion_GE(p):
-  'condicion: LE'
-   P[0]= Node(menor_que,p[1],p[])
-		
-#---------------relacion---------------------------------------
-def p_relacion_condicion(p):
-   ' relacion: expre condicion expre'
-     p[0] = Node(relacion_condicion, p[1],p[3])
+#  ---------------------------------------------------------------
+#  EXPRE
+#  ---------------------------------------------------------------
 
-def p_relacion_and(p):
-	'relacion: relacion AND relacion'
-	 p[0] = Node(And,p[1],p[3])
+def p_expre_mas(p):
+	'expre: expre MAS expre'
+	p[0]= Node('+',[p[1],p[3]])
 
-def p_relacion_or(p):
-	'relacion: relacion OR relacion'
-	 p[0] = Node(OR,p[1],p[3])
+def p_expre_menos(p):
+	'expre: expre MENOS expre'
+	p[0]= Node('-',[p[1],p[3]])
+ 
+def p_expre_mul(p):
+	'expre: expre MUL expre'
+	p[0]= Node('*',[p[1],p[3]])
 
-def p_relacion_not(p):
-	'relacion: NOT relacion'
-	p[0] =node(NOT,p[2],p[])
+def p_expre_div(p):
+	'expre: expre DIV expre'
+	p[0]= Node('/',[p[1],p[3]])
+ 
+ def p_expre_menosu(p):
+	'expre: MENOS expre'
+	p[0]= Node('umenos',[p[2]])
 
-def p_relacion_parent(p):
-	'relacion: PARI relacion PARD'
-	p[0] =node(relacion,p[2],p[])	
-#----------------------------------------------------------------------
+def p_expre_masu(p):
+	'expre: MAS expre'
+	p[0]= Node('umas',[p[2]])
+
+def p_expre_call(p):
+	'expre: ID PARI exprelist PARD'
+	#TODO
+	p[0] = Node('call',[p[3]],p[1])
+
+def p_expre_id(p):
+	'expre: ID'
+	p[0] = Node('id', [], p[1])
+	p[0].value = p[1]
+
+def p_expre_array(p):
+	'expre: ID CORI exprelist CORD'
+	p[0] = Node('array',[p[3]],p[1])
+
+	#indices enteros
+	if hasattr(p[3],'typ'):
+		if p[3].typ != 'int':
+			print "#Error# El indice del array debe ser un valor entero '%s'" % f.name
+		else:
+			p[0].typ = p[3].typ
+
+def p_expre_fnum(p):
+	'expre: FNUM'
+	p[0]= Node('numero_f',p[1])
+
+def p_expre_inum(p):
+	'expre: INUM'
+	p[0]= Node('numero',p[1])
+
+def p_expre_cast_int(p):
+	'expre: INT PARI expre PARD'
+	p[0] = Node('cast_int',[p[3]],p[1])
+
+def p_expre_cast_float(p):
+	'expre: FLOAR PARI expre PARD'
+	p[0] = Node('cast_float',[p[3]],p[1])
 
 
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+#  PARSER
+# -----------------------------------------------------------------------------
 
-
-
-
-
-	pass
 # Error rule for syntax errors
 def p_error(p):
     print "Syntax error in input!"
