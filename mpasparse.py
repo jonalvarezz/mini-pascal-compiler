a094e19d1c6e8e68e9eb01279b55e6995e786dfb
Jona <jalvarez@tamarindolabs.com>
2012-10-07 16:59:39 -0500

    Correccion Lineas para el uso de Begin y End

--- mpasparse.py
+++ mpasparse.py
@@ -60,7 +60,7 @@ precedence =(
 	('left', 'AND'),
     ('left', 'NOT'),
     ('left', 'MAS', 'MENOS'),
-    ('left', 'MUL', 'DIV'),
+    ('left', 'MUL', 'DIV'),   
 	('right', 'ELSE'),
 )
 
@@ -81,7 +81,7 @@ def p_programa_1(p):
 #  ---------------------------------------------------------------
 
 def p_funcion(p):
-	'funcion : FUN ID argumento locales BEGIN sentencia END'
+	'funcion : FUN ID argumento locales linea'
 	p[0] = Node( 'funcion', [p[2], p[3], p[4], p[6]] )
 
 #  ---------------------------------------------------------------
@@ -90,11 +90,11 @@ def p_funcion(p):
 
 def p_argumento_0(p):
 	'argumento : PARI PARD'
-	p[0] = Node( 'void argumento' )
+	p[0] = Node( 'void argumento', [] )
 
 def p_argumento_1(p):
 	'argumento : PARI declaracion PARD'
-	p[0] = Node( 'argumento', [p[2]] )
+	p[0] = p[2]
 
 #  ---------------------------------------------------------------
 #  DECLARACIONES
@@ -114,11 +114,11 @@ def p_declaracion_1(p):
 
 def p_locales_0(p):
 	'locales : loclist PCOMA'
-	p[0] = Node( 'locales', [p[1]] )
+	p[0] = p[1]
 
 def p_locales_1(p):
 	'locales : vacio'
-	p[0] = Node( 'void locales' )
+	p[0] = Node( 'void locales', [] )
 
 #  ---------------------------------------------------------------
 #  LOCLIST
@@ -127,6 +127,7 @@ def p_locales_1(p):
 def p_loclist_0(p):
 	'loclist : dec'
 	p[0] = Node( 'loclist_dec', [p[1]] )
+	p[0].name = p[1].name
 
 def p_loclist_1(p):
 	'loclist : funcion'
@@ -149,21 +150,16 @@ def p_dec(p):
 	p[0] = Node( 'dec', [p[1], p[3]] )
 
 #  ---------------------------------------------------------------
-#  SENTENCIA
+#  LINEAS
 #  ---------------------------------------------------------------
 
-def p_sentencia_0(p):
-	'sentencia : linea pcoma_op'
-	p[0] = Node( 'sentencia', [p[1]] )
+def p_lineas_0(p):
+	'lineas : linea'
+	p[0] = Node( 'linea', [p[1]] )
 
-def p_sentencia_1(p):
-	'sentencia : sentencia PCOMA linea'
+def p_lineas_1(p):
+	'lineas : lineas PCOMA linea'
 	p[0] = p[1].append(p[3])
-
-def p_pcoma_op(p):
-	'''pcoma_op : PCOMA
-				| vacio'''
-	pass
 
 #  ---------------------------------------------------------------
 #  LINEA
@@ -182,15 +178,15 @@ def p_linea_2(p):
 
 def p_linea_3(p):
 	'linea : PRINT PARI STRING PARD'
-	p[0] = Node( 'print', [p[3]] )
+	p[0] = Node( 'print', [], p[3] )
 
 def p_linea_4(p):
 	'linea : ID ASIG expre'
-	p[0] = Node( ':=', [p[3], p[1]] )
+	p[0] = Node( ':=', [p[1], p[3]] )
 
 def p_linea_5(p):
 	'linea : RETURN expre'
-	p[0] = Node( 'return', [p[3]] )
+	p[0] = Node( 'return', [p[2]], p[1] )
 
 def p_linea_6(p):
 	'linea : ID PARI exprelist PARD'
@@ -199,26 +195,30 @@ def p_linea_6(p):
 
 def p_linea_7(p):
 	'linea : SKIP'
-	p[0] = Node( 'skip', [], [p[1]] )
+	p[0] = Node( 'skip', [p[1]] )
 
 def p_linea_8(p):
 	'linea : BREAK'
-	p[0] = Node( 'break', [], [p[1]] )
+	p[0] = Node( 'break', [], p[1] )
 
 def p_linea_9(p):
-	'linea : WHILE relacion DO BEGIN sentencia END'
-	p[0] = Node( 'while', [p[2], p[4]] )
+	'linea : WHILE relacion DO linea'
+	p[0] = Node( 'while', [p[2], p[5]] )
 
 def p_linea_10(p):
-	'linea : IF relacion THEN sentencia else_r'
+	'linea : IF relacion THEN linea else_r'
 	p[0] = Node( 'if', [p[2], p[4], p[5]] )
+
+def p_linea_11(p):
+	'linea : BEGIN lineas END'
+	p[0] = p[2]
 
 #  ---------------------------------------------------------------
 #  ELSE
 #  ---------------------------------------------------------------
 
 def p_else_0(p):
-	'else_r : ELSE sentencia'
+	'else_r : ELSE linea'
 	p[0] = Node( 'else', [p[2]] )
 
 def p_else_1(p):
@@ -235,7 +235,7 @@ def p_location_1(p):
 
 def p_location_2(p):
 	'location : ID CORI expre CORD'
-	p[0] = Node('id[]',[],p[1])
+	p[0] = Node('id[]',[p[3]],p[1])
 
 #  ---------------------------------------------------------------
 #  RELACION
@@ -275,11 +275,11 @@ def p_relacion_or(p):
 
 def p_relacion_not(p):
 	'relacion : NOT relacion'
-	p[0] =node('not',[p[2]])
+	p[0] = Node('not',[p[2]])
 
 def p_relacion_parent(p):
 	'relacion : PARI relacion PARD'
-	p[0] =node('relacion',[p[2]])
+	p[0] = Node('relacion',[p[2]])
 
 #  ---------------------------------------------------------------
 #  TYPE
@@ -295,11 +295,11 @@ def p_type_i(p):
 
 def p_type_fa(p):
 	'type : FLOAT CORI expre CORD'
-	p[0] = Node('FLOAT_Array',[],p[3])
+	p[0] = Node('FLOAT_Array', [p[3]])
 
 def p_type_ia(p):
 	'type : INT CORI expre CORD'
-	p[0] = Node('INT_Array',[],p[3])
+	p[0] = Node('INT_Array', [p[3]])
 
 #  ---------------------------------------------------------------
 #  EXPRLIST
@@ -344,7 +344,7 @@ def p_expre_masu(p):
 def p_expre_call(p):
 	'expre : ID PARI exprelist PARD'
 	#TODO
-	p[0] = Node('call',[p[3]],p[1])
+	p[0] = Node('call1',[p[3]],p[1])
 
 def p_expre_id(p):
 	'expre : ID'
