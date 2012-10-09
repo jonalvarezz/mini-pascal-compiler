56644d4f9417e6492c884372fbb1c8e75fcd0624
daniel <tamarindodn@hotmail.com>
2012-10-08 23:39:10 -0500

    pruevas modificadas

--- Pruebas/test5.pas
+++ Pruebas/test5.pas
@@ -1,8 +1,11 @@
 FUN main( arg:FLOAT, arg2:INT[189] )
 	n:INT;
 BEGIN
-	PRINT( "Una cadena de text" );
-	PRINT( "1 Una cadena de text" );
-	
-	WRITE( 5 + 2 / 2 )
+	 
+     IF 3 > 1 THEN 
+     WRITE( 5 + 2 / 7 )
+     ELSE
+     WRITE( 5 + 2 / 7 )
+     ;
+     WRITE( 5 + 2 / 7 )
 END
