f784605bd685ce9b489820a654d790f097d9a421
Jona <jalvarez@tamarindolabs.com>
2012-11-16 03:45:45 -0500

    Clase de simbolos terminada

--- symtab.py
+++ symtab.py
@@ -16,18 +16,27 @@ class Symbol:
     def append(self,num):
         self.numpar.append(num)
 
+# Crea un nueva tabla
 def new_scope():
 	global current
 	current = {}
 	_scopes.append(current)
 	return current
 
+# Elimina la tabla actual y restaura la anterior.
 def pop_scope():
 	global current
 	r = _scopes.pop()
 	current = _scopes[-1]
 	return r
 
+# Busca un simbolo en la tabla actual
+def is_symbol( name ) :
+    if current.get( name, False ):
+        return True
+    return False
+
+# Busca un simbolo en todas las tablas de simbolos
 def get_symbol(name,level = 0,attr = None):
 	for i in range(len(_scopes)-(level+1),-1,-1):
 		s = _scopes[i]
@@ -42,6 +51,7 @@ def get_symbol(name,level = 0,attr = None):
             pass
     return None
 
+# Agrega un nuevo simbolo
 def add_symbol(name):
     s = Symbol()
     s.name = name
