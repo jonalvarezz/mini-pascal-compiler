7cd3e4e96552fbb00c36427b837e496df37343cd
unknown <tamarindodn@hotmail.com>
2012-11-18 18:12:44 -0500

    mpastype y documentacion mparse

--- mpasparse.py
+++ mpasparse.py
@@ -3,7 +3,7 @@ import ply.yacc as yacc
 import sys
 from mpaslex import tokens
 import symtab
-
+import mpastype
 #  ---------------------------------------------------------------
 #  ABSTRACT SYNTAX TREE - NODE
 #  ---------------------------------------------------------------
@@ -31,11 +31,10 @@ class Node:
 
 def dump_tree(node, indent = ""):
 
-	if not hasattr(node, "datatype"):
+	if not hasattr(node, "typ"):
 		datatype = ""
 	else:
-		datatype = node.datatype
-		print( ' %s' % datatype )
+		datatype = node.typ
 	try:
 		if not node.leaf:
 			print ("%s %s  %s" % (indent, node.name, datatype))
@@ -97,12 +96,12 @@ def p_funcion(p):
 	'funcion : fundecl ID PARI argumento PARD locales BEGIN lineas END'
 	p[0] = Node( 'funcion', [p[4], p[6], p[8]], p[2] )
 	# Elimina la tabla de simbolos actual, y restaura la anterior.
-	#symtab.pop_scope()
+#	symtab.pop_scope()
 
 def p_fundecl(p):
 	'fundecl : FUN'
 	# Crea una nueva tabla de simbolos
-	#symtab.new_scope()
+	symtab.new_scope()
 
 #  ---------------------------------------------------------------
 #  ARGUMENTO
@@ -174,6 +173,15 @@ def p_loc_1(p):
 def p_dec(p):
 	'dec : ID DPUN type'
 	p[0] = Node( 'declaracion', [p[3]], p[1] )
+	p[0].name = p[1]
+	p[0].value = p[1]
+#	p[0].typ=p[3].typ
+#	a=symtab.banf(p[0].name,p[3].typ)
+#	if a :
+#		print "#Error# redeclaracion de identificador '%s' en linea %i" % (p[0].name,a.lineno)
+
+
+
 
 #  ---------------------------------------------------------------
 #  LINEAS
@@ -211,6 +219,22 @@ def p_linea_3(p):
 def p_linea_4(p):
 	'linea : ID ASIG expre'
 	p[0] = Node( ':=', [p[3]], p[1] )
+	p[0].assign = 1
+#	print "\n* %s *\n" % dir(p[0].name)
+#	a=symtab.find(p[1].name)
+#	if hasattr(p[1], "typ"):
+#		typ=p[1].typ
+#	else:
+#		typ=None
+
+#	s=symtab.banf2(p[1].name,typ)
+#	if not s:
+#		pass
+		###print "#Error# Assign '%s' error de tipos" % (p[1].name)
+
+#	if a:
+#		print "#Error# variable no declarada '%s' en la linea %i " % (p[1].name,a.lineno)
+
 
 def p_linea_5(p):
 	'linea : RETURN expre'
@@ -251,14 +275,27 @@ def p_else_1(p):
 #  ---------------------------------------------------------------
 #  LOCATION
 #  ---------------------------------------------------------------
+#  bug detectado!!! no entra nunca por aki la funcion read coje id en ves de alguna de estas
 
 def p_location_1(p):
 	'location : ID'
 	p[0] = Node('id',[],p[1])
+	p[0].name = p[1]
+	p[0].value = p[1]
+
+# por el error ya mencionado no se pudo comprobar el funcionamiento
 
 def p_location_2(p):
 	'location : ID CORI expre CORD'
 	p[0] = Node('id[]',[p[3]],p[1])
+	p[0].name = p[1]
+	p[0].value = p[1]
+
+	if hasattr(p[3],'typ'):
+		if p[3].typ != 'int':
+			print "#Error# El indice de un vector debe ser entero '%s'" % f.name
+		else:
+			p[0].typ = p[3].typ
 
 #  ---------------------------------------------------------------
 #  RELACION
@@ -311,18 +348,22 @@ def p_relacion_parent(p):
 def p_type_f(p):
 	'type : FLOAT'
 	p[0] = Node('type_float',[], p[1])
+	p[0].typ = "float_type"
 
 def p_type_i(p):
 	'type : INT'
 	p[0] = Node('type_int',[],p[1])
+	p[0].typ = "int_type"
 
 def p_type_fa(p):
 	'type : FLOAT CORI expre CORD'
 	p[0] = Node('type_float_Array', [p[3]])
+ 	p[0].typ="int["+str(p[3].value)+"]_type"
 
 def p_type_ia(p):
 	'type : INT CORI expre CORD'
 	p[0] = Node('type_int_Array', [p[3]])
+	p[0].typ="int["+str(p[3].value)+"]_type"
 
 #  ---------------------------------------------------------------
 #  EXPRLIST
@@ -336,6 +377,7 @@ def p_exprelist_coma(p):
 def p_exprelist_(p):
 	'exprelist : expre'
 	p[0] = p[1]
+# y las funciones sin parametros hay exprelist vacias ????''
 	
 #  ---------------------------------------------------------------
 #  EXPRE
@@ -360,46 +402,103 @@ def p_expre_div(p):
 def p_expre_menosu(p):
 	'expre : MENOS expre'
 	p[0]= Node('umenos',[p[2]])
+#	requiere asignacion de tipo pero todavia no se como implementarla
 
 def p_expre_masu(p):
 	'expre : MAS expre'
 	p[0]= Node('umas',[p[2]])
+#	requiere asignacion de tipo pero todavia no se como implementarla
 
 def p_expre_call(p):
 	'expre : ID PARI exprelist PARD'
 	p[0] = Node( 'call', [p[3]], p[1] )
+	f=symtab.find(p[1])
+	print f
+	args = len(p[3].children)
+	print ("la funcion tiene '%s' argumentos" %args)
+	if 	p[3].leaf:
+		args += 1
+
+	if not f:
+		print "Error Funcion no declarada '%s'  " % (p[1]),
+#		lf = get_linea(p[1])
+#		print "en la linea '%s'" % lf
+	else:
+		if args != len(f.numpar):
+			print "#Error# Numero de argumentos erroneo en '%s'" % f.name
+#		else:
+#			if 	p[3].leaf:
+				#print "adadsdsad: ", p[3].typ , ":::" , f.name,f.numpar
+#				if p[3].typ != f.numpar[0]:
+#					print "#Error# Tipos de argumentos erroneo en'%s'" % f.name
+#				else:
+#					for i in range(0,len(p[3].children)):
+#						if hasattr(p[3].children[i],'typ'):
+#							if p[3].children[i].typ != f.numpar[i+1]:
+#								print "#Error# Tipos de argumentos erroneo en'%s'" % f.name
+#								break
+#	if hasattr(p[3],'typ'):
+#		p[0] = Node('',[p[3]],p[1])
+#		p[0].name = p[1]
+#		p[0].value = p[1]
+#		p[0].call = 1
+#		p[0].typ = p[3].typ
+#	else:
+#		p[0] = Node('',[p[3]],p[1])
+#		p[0].call = 1
+#		p[0].name = p[1]
+#		p[0].value = p[1]
 
 def p_expre_id(p):
 	'expre : ID'
 	p[0] = Node('id', [], p[1])
 	p[0].value = p[1]
+	# si esta en la tabla de simbolos
+	f = symtab.get_symbol(p[1])
+	if f:
+		print "\n* %s *\n" % dir(f)
+#		p[1].typ = f
+	else:
+		print ("#ERROR# no esta declarada %s" %p[0].value)	
 
 def p_expre_array(p):
 	'expre : ID CORI expre CORD'
 	p[0] = Node('array',[p[3]],p[1])
-
 	#indices enteros
-	if hasattr(p[3],'typ'):
-		if p[3].typ != 'int':
-			print ("#Error# El indice del array debe ser un valor entero '%s'" % f.name)
-		else:
-			p[0].typ = p[3].typ
+	if hasattr(p[3],'typ') & hasattr(p[3],'value'):
+			if p[3].typ != 'int_type':
+				print ("#Error# El indice del array debe ser un valor entero '%s'" % f.name)
+			else:
+				p[0].typ = "int["+str(p[3].value)+"]_type"
+	else:
+		print ("#Error# no se le encuentra valor")
+	
+
 
 def p_expre_fnum(p):
 	'expre : FNUM'
 	p[0]= Node('numero_f',[], p[1])
+	p[0].value = p[1]
+	p[0].typ = "float_type"
 
 def p_expre_inum(p):
 	'expre : INUM'
 	p[0]= Node('numero',[],p[1])
+	p[0].value = p[1]
+	p[0].typ = "int_type"
 
 def p_expre_cast_int(p):
 	'expre : INT PARI expre PARD'
 	p[0] = Node('cast_int',[p[3]],p[1])
+	p[0].value = p[p[3].value]
+	p[0].typ = "int_type"
+
 
 def p_expre_cast_float(p):
 	'expre : FLOAT PARI expre PARD'
 	p[0] = Node('cast_float',[p[3]],p[1])
+	p[0].value = p[p[3].value]
+	p[0].typ = "float_type"
 
 
 # -----------------------------------------------------------------------------
